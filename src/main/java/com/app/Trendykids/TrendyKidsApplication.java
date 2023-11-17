72deef9f13c56ceb7bca21209528578e60e91df3
Stanislav2509 <staso03@abv.bg>
2023-11-17 02:28:51 +0200

    start

---
+++ src/main/java/com/app/Trendykids/TrendyKidsApplication.java
@@ -0,0 +1,13 @@
+package com.app.Trendykids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendyKidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendyKidsApplication.class, args);
+	}
+
+}
